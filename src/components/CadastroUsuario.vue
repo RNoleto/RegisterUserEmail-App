30b5306636d34813d84ea26f0bc17b9aefc3c190
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2024-03-27 21:53:16 -0300

    criação do componente de formulario de registro de usuario

---
+++ src/components/CadastroUsuario.vue
@@ -0,0 +1,37 @@
+<!-- CadastroUsuario.vue -->
+
+<template>
+  <div class="container">
+    <h2>Cadastro de Usuário</h2>
+    <form class="form">
+      <div class="form-group">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" v-model="usuario.nome" required />
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="usuario.email" required />
+      </div>
+      <div class="form-group">
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" v-model="usuario.senha" required />
+      </div>
+      <button type="submit">Cadastrar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario: {
+        nome: "",
+        email: "",
+        senha: "",
+      },
+    };
+  },
+};
+</script>
+
