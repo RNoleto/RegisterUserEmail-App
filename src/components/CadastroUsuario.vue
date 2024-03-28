b2d9b292aedd6284773a32395131842f6c7fd0d9
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2024-03-28 20:43:00 -0300

    integração do api

--- src/components/CadastroUsuario.vue
+++ src/components/CadastroUsuario.vue
@@ -3,18 +3,23 @@
 <template>
   <div class="container">
     <h2>Cadastro de Usuário</h2>
-    <form class="form">
+    <form @submit.prevent="registerUser" class="form">
       <div class="form-group">
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" v-model="usuario.nome" required />
+        <label for="name">Nome:</label>
+        <input type="text" id="name" v-model="formData.name" required />
       </div>
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" id="email" v-model="usuario.email" required />
+        <input type="email" id="email" v-model="formData.email" required />
       </div>
       <div class="form-group">
-        <label for="senha">Senha:</label>
-        <input type="password" id="senha" v-model="usuario.senha" required />
+        <label for="password">Senha:</label>
+        <input
+          type="password"
+          id="password"
+          v-model="formData.password"
+          required
+        />
       </div>
       <button type="submit">Cadastrar</button>
     </form>
@@ -22,15 +27,29 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      usuario: {
-        nome: "",
+      formData: {
+        name: "",
         email: "",
-        senha: "",
+        password: "",
       },
     };
+  },
+  methods: {
+    registerUser() {
+      axios
+        .post("http://127.0.0.1:8000/api/register", this.formData)
+        .then((response) => {
+          window.alert(`Cadastro de ${this.formData.name} feito com sucesso!`);
+        })
+        .catch((error) => {
+          window.alert("Erro ao cadastrar usuario!");
+          console.error("Erro ao cadastrar usuario:", error.response.data);
+        });
+    },
   },
 };
 </script>
