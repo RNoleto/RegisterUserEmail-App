1ff7592ab3d5022108c7f035e49a3deb4aa6c987
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2024-03-29 15:18:12 -0300

    ajuste de layout basico

--- src/components/CadastroUsuario.vue
+++ src/components/CadastroUsuario.vue
@@ -1,7 +1,6 @@
-<!-- CadastroUsuario.vue -->
-
 <template>
   <div class="container">
+    <img src="../assets/img/login.jpg" alt="imagem de login" />
     <h2>Cadastro de Usuário</h2>
     <form @submit.prevent="registerUser" class="form">
       <div class="form-group">
@@ -53,4 +52,52 @@ export default {
   },
 };
 </script>
+<style>
+img {
+  width: 250px;
+  height: auto;
+}
+h2 {
+  color: #007dfe;
+}
+.container {
+  padding: 10px;
+  border-radius: 15px;
+  border: 1px solid #007dfe;
+}
+.form-group {
+  display: flex;
+  flex-direction: column;
+}
+label {
+  color: #007dfe;
+  display: flex;
+  flex-direction: flex-start;
+}
+
+input {
+  border: none;
+  border-radius: 10px;
+  background-color: #a6c9ff;
+  padding: 5px;
+  height: 25px;
+  transition: border-color 0.3s ease; /* Adiciona transição para a mudança de cor da borda */
+}
+input:focus {
+  border-color: #007dfe; /* Muda a cor da borda quando o input é focado */
+}
+button {
+  border: none;
+  background: #007dfe;
+  color: #e9e9e9;
+  width: 100%;
+  margin-top: 10px !important;
+  transition: background-color 0.3s ease, color 0.3s ease; /* Adiciona transições suaves para a mudança de cor do botão */
+}
+button:hover {
+  border: none;
+  background: #a6c9ff;
+  color: #2f2f30;
+}
+</style>
 
